358b122202ec5c6ca40ef660563f0163cc54b0be
Gary Chen <garychen.whc@gmail.com>
2019-08-27 17:57:18 +0000

    async & sync

---
+++ asyncBreeds.js
@@ -0,0 +1,20 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+
+  console.log('breedDetailsFromFile: Calling readFile...');
+
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+
+    console.log('Callback: I have the data!')
+
+    if (!error) {
+      callback(data); 
+    } else {
+      callback(undefined);
+    }
+  });
+}
+
+module.exports = breedDetailsFromFile;
